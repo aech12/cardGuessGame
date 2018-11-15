f0691b5194874f3e629ebd553b1991352d4a21cf
Alejandro Howez <aech-12@hotmail.com>
2018-11-15 13:46:53 -0300

    Updates

---
+++ precache-manifest.93c1f6df86e27a2118beeba4b185ed2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e5f5dd6ab98f7189abf",
+    "url": "/cardGuessGame/static/js/runtime~main.8e5f5dd6.js"
+  },
+  {
+    "revision": "af2d36374bb799f03d45",
+    "url": "/cardGuessGame/static/js/main.af2d3637.chunk.js"
+  },
+  {
+    "revision": "8df7078f39324016d415",
+    "url": "/cardGuessGame/static/js/1.8df7078f.chunk.js"
+  },
+  {
+    "revision": "af2d36374bb799f03d45",
+    "url": "/cardGuessGame/static/css/main.16f5c599.chunk.css"
+  },
+  {
+    "revision": "81cb1a7eba7881efebcc64862d91cf58",
+    "url": "/cardGuessGame/index.html"
+  }
+];
