5ffa3b413e0b336b29f99f7c645d7715964ea23b
Alejandro Howez <aech-12@hotmail.com>
2018-11-24 23:33:27 -0300

    Updates

---
+++ precache-manifest.63be2d3975485f72274606e920033d42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a71d35c1fbb3241cd8",
+    "url": "/tictactoe/static/js/runtime~main.a6a71d35.js"
+  },
+  {
+    "revision": "40f19a37aa3f984fb866",
+    "url": "/tictactoe/static/js/main.40f19a37.chunk.js"
+  },
+  {
+    "revision": "8df7078f39324016d415",
+    "url": "/tictactoe/static/js/1.8df7078f.chunk.js"
+  },
+  {
+    "revision": "40f19a37aa3f984fb866",
+    "url": "/tictactoe/static/css/main.83ad05f5.chunk.css"
+  },
+  {
+    "revision": "7c19a661078efb17108f0d2720acf00c",
+    "url": "/tictactoe/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/cardGuessGame/precache-manifest.93c1f6df86e27a2118beeba4b185ed2b.js"
+  "/tictactoe/precache-manifest.63be2d3975485f72274606e920033d42.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cardGuessGame/index.html", {
+workbox.routing.registerNavigationRoute("/tictactoe/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
